fd52e533b6009f3238b7141799343fd721c9680e
efraespada <efraespada@gmail.com>
2018-02-15 00:25:14 +0100

    unicode chars on xml crash

--- src/main/groovy/AES.java
+++ src/main/groovy/AES.java
@@ -1,78 +1,45 @@
 
+import org.apache.commons.codec.binary.Base64;
+
 import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.UnsupportedEncodingException;
-import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
 public class AES {
-	
-	public static SecretKey generateKey(String key) throws NoSuchAlgorithmException {
-		MessageDigest digest = MessageDigest.getInstance("SHA-1");
-	    byte[] passphrase = null;
+
+    private static final String CHARSET = "UTF-8";
+    private static final String MSG_D_ALGORITHM = "SHA-1";
+    private static final String ALGORITHM = "AES";
+    private static final String TRANSFORMATION = "AES/ECB/PKCS5Padding";
+
+
+    private static SecretKey generateKey(String key) throws NoSuchAlgorithmException {
+		MessageDigest digest = MessageDigest.getInstance(MSG_D_ALGORITHM);
 		try {
-			passphrase = digest.digest(key.getBytes("UTF-8"));
-		} catch (UnsupportedEncodingException e) {
+            byte[] acd = Arrays.copyOf(digest.digest(key.getBytes(CHARSET)), 16);
+            return new SecretKeySpec(acd, ALGORITHM);
+        } catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
-	    
-	    passphrase = Arrays.copyOf(passphrase, 16);
-	    return new SecretKeySpec(passphrase, "AES");
+	    return null;
     }
 	
     public static String encrypt(String message, String key) throws Exception {
-    	byte[] data = message.getBytes("UTF-8");
-        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+    	byte[] data = Base64.encodeBase64String(message.getBytes(CHARSET)).getBytes();
+        Cipher cipher = Cipher.getInstance(TRANSFORMATION);
         cipher.init(Cipher.ENCRYPT_MODE, generateKey(key));
-        byte[] encryptData = cipher.doFinal(data);
-        
-        // System.out.println(byteArrayToHexString(encryptData));
-
-        return byteArrayToHexString(encryptData);
+        return new String(cipher.doFinal(data));
     }
 
-    public static String decrypt(String v, String key) throws Exception {
-    	byte[] tmp = hexStringToByteArray(v);
-        SecretKeySpec spec = new SecretKeySpec(generateKey(key).getEncoded(), "AES");
-        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+    public static String decrypt(String value, String key) throws Exception {
+        SecretKeySpec spec = new SecretKeySpec(generateKey(key).getEncoded(), ALGORITHM);
+        Cipher cipher = Cipher.getInstance(TRANSFORMATION);
         cipher.init(Cipher.DECRYPT_MODE, spec);
-        String result;
-        try {
-        	result = new String(cipher.doFinal(tmp), StandardCharsets.UTF_8);
-        } catch (IllegalBlockSizeException e) {
-        	result = v;
-        }
-        // System.out.println(result);
-        return result;
-
-    }
-    
-    public static byte[] hexStringToByteArray(String s) {
-        int len = s.length();
-        byte[] data = new byte[len/2];
-
-        for(int i = 0; i < len; i+=2){
-            data[i/2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i+1), 16));
-        }
-
-        return data;
+        return new String(cipher.doFinal(Base64.encodeBase64String(value.getBytes()).getBytes()), CHARSET);
     }
 
-	final protected static char[] hexArray = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-	public static String byteArrayToHexString(byte[] bytes) {
-	    char[] hexChars = new char[bytes.length*2];
-	    int v;
-	
-	    for(int j=0; j < bytes.length; j++) {
-	        v = bytes[j] & 0xFF;
-	        hexChars[j*2] = hexArray[v>>>4];
-	        hexChars[j*2 + 1] = hexArray[v & 0x0F];
-	    }
-	
-	    return new String(hexChars);
-	}
 }
