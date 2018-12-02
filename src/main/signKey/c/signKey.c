a65046d73dda9bdd50b4b44a507d8f86270ec508
Efraín Espada <efrainespada@Efrains-Mac-mini.local>
2018-12-02 03:25:14 +0100

    ...

--- src/main/signKey/c/signKey.c
+++ src/main/signKey/c/signKey.c
@@ -2,6 +2,6 @@
 #include <stdio.h>
 
 JNIEXPORT jstring JNICALL Java_CredentialUtils_sign(JNIEnv *env, jclass clazz, jstring key) {
-    printf("Hello Wdorld!\n");
+    printf("Hello World!\n");
     return key;
 }
